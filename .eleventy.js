c67336d5b91cba6f92c80f7052de532f3234132d
Andy Bell <andy@set.studio>
2023-05-16 16:59:02 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+module.exports = config => {
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
+  // Plugins
+  config.addPlugin(rssPlugin);
+
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection('blog', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
+
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
+
+  // Pass through fonts and images
+  config.addPassthroughCopy('./src/fonts');
+  config.addPassthroughCopy('./src/images');
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  };
+};
