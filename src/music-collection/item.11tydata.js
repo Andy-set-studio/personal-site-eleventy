e1b71b4cbdcbe9de922450b030ca8f4b695df946
Andy Bell <andy@set.studio>
2023-05-19 10:08:01 +0100

    Music collection tweaks

--- src/music-collection/item.11tydata.js
+++ src/music-collection/item.11tydata.js
@@ -4,6 +4,7 @@ module.exports = {
   eleventyComputed: {
     items: data => {
       return randomFilter(data.collections.music, 8);
-    }
+    },
+    metaTitle: data => `${data.item.name} - ${data.item.artist} - Music Collection`
   }
 };
