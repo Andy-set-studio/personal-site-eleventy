4c7d9daad41629a2bdb230044f2447b908e976ca
Andy Bell <andy@set.studio>
2023-07-06 12:13:42 +0100

    Change networks

--- src/_data/socialNetworks.js
+++ src/_data/socialNetworks.js
@@ -1,14 +1,18 @@
 module.exports = [
   {
-    text: 'Bluesky (<code>@andy-bell.co.uk</code>)',
-    url: 'https://bsky.app/profile/andy-bell.co.uk'
+    text: 'Bluesky (<code>@bell.bz</code>)',
+    url: 'https://bsky.app/profile/bell.bz'
   },
   {
     text: 'Mastodon',
     url: 'https://bell.bz/@andy'
   },
   {
+    text: 'Threads',
+    url: 'https://www.threads.net/@belldotbz'
+  },
+  {
     text: 'Twitter',
-    url: 'https://twitter.com/piccalilli_'
+    url: 'https://twitter.com/belldotbz'
   }
 ];
