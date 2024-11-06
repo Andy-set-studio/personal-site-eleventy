b691cc57e44ac0c070cf2d93b037c6a5bcbb7188
Andy Bell <andy@set.studio>
2024-11-06 10:23:14 +0000

    Update socialNetworks.js

--- src/_data/socialNetworks.js
+++ src/_data/socialNetworks.js
@@ -6,10 +6,6 @@ module.exports = [
     url: 'https://bsky.app/profile/bell.bz'
   },
   {
-    text: 'Twitter (<code>I never post here</code>)',
-    url: 'https://twitter.com/belldotbz'
-  },
-  {
     text: 'Threads (<code>I never post here</code>)',
     url: 'https://www.threads.net/@belldotbz'
   }
